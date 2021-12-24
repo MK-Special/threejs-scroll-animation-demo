eff4d5de7cba40de305cb94b9be03a3ce9f2d484
MK <mkspecialbusiness@gmail.com>
2021-12-24 17:04:13 +0200

    Fixed images not appearing

--- main.js
+++ main.js
@@ -65,12 +65,12 @@ Array(200).fill().forEach(addStar);
 
 // Background
 
-const spaceTexture = new THREE.TextureLoader().load("space.jpg");
+const spaceTexture = new THREE.TextureLoader().load("/assets/img/space.jpg");
 scene.background = spaceTexture;
 
 // Avatar
 
-const jeffTexture = new THREE.TextureLoader().load("first.jpg");
+const jeffTexture = new THREE.TextureLoader().load("/assets/img/first.jpg");
 
 const jeff = new THREE.Mesh(
     new THREE.BoxGeometry(3, 3, 3),
@@ -81,8 +81,8 @@ scene.add(jeff);
 
 // Moon
 
-const moonTexture = new THREE.TextureLoader().load("moon.jpg");
-const normalTexture = new THREE.TextureLoader().load("normal.jpg");
+const moonTexture = new THREE.TextureLoader().load("/assets/img/moon.jpg");
+const normalTexture = new THREE.TextureLoader().load("/assets/img/normal.jpg");
 
 const moon = new THREE.Mesh(
     new THREE.SphereGeometry(3, 32, 32),
